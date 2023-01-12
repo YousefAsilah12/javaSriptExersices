b1cb24d7708617eadfdce76fd90ebdd4d402ff79
Yous3f <yasilah4@gmail.com>
2023-01-12 09:53:05 +0200

    7.3-.4

---
+++ model/7/7.3.js
@@ -0,0 +1,8 @@
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end 
+
+
+const fruits = ["Banana", "Orange", "Lemon",
+"Apple", "Mango"];
+
+const fruitsCopy = fruits.slice(1,3);
+console.log(fruitsCopy);
---
+++ model/7/7.4.js
@@ -0,0 +1,7 @@
+arr1=["lemon","kiwi"];
+const fruits = ["Banana", "Orange", "Apple",
+"Mango"];
+
+
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
