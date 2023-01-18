bd827faaded93bb24078ebd04b07c7029395c7d9
Yous3f <yasilah4@gmail.com>
2023-01-18 14:39:55 +0200

    17.2

--- model/17/17.2.js
+++ model/17/17.2.js
@@ -6,5 +6,64 @@
 // 0 
 // 1
 
-// Block3
+// block 5 
+// 10
+// 20 //private virable
+// 10
 
+
+// block 6 
+// 10 //global
+// 20 //inner //also not affect to out side 
+// 30 //private not affect to out side 
+// 20 //inner
+// 10 //global
+
+
+// block 7
+
+var x;
+x = 10;
+
+function test() {
+  var x; //x is declared here with no value
+  if (x > 20) {
+    x = 50;
+  }
+  console.log(x); //no value undefined
+
+}
+test();
+
+console.log("--");
+
+// block 8
+function test() {
+  var x, y;
+  if (false) {
+    x = 50; // deafult is true will not change value of x 
+  }
+  console.log(x); //undefined
+
+  console.log(y); //not declared yet undefined
+
+  y = 100;
+  console.log(y); //100
+}
+test();
+
+
+console.log("--");
+
+function test() {
+  foo();
+  bar(); //declared befor expression function its need to after  error
+
+  function foo() {
+    console.log('foo');
+  }
+  var bar = function () {
+    console.log('bar');
+  }
+}
+test();
