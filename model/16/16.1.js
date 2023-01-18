3e4d7595354b2d35b519764e5c6ff155c1db9b5d
Yous3f <yasilah4@gmail.com>
2023-01-18 10:52:31 +0200

    16

---
+++ model/16/16.1.js
@@ -0,0 +1,33 @@
+// block 1 
+//  a -> undefind
+// foo -->2 
+
+// block 2 
+// its will print the full name 
+// and test() its undefind 
+
+
+
+// block 3 
+// if we add to funcb() console.log it will return 1 
+// console.log(typeof a)
+// console.log(typeof b) 
+// the a and b not know to out side the functiona
+
+
+
+// block 4
+// the return will be to the latest update of function 
+// because its a dunction dosent matter were its placed 
+
+
+// block 5
+// funcD1 its not return or log to screen nothing
+// clg(d) it will return 1 because the function has changed the value of d 
+//clg(e) wil bring an error because the e has declared inside the function
+
+
+// block 6
+//first log the log to screen the text but the f is undefined
+//the secound will print also the value of f because its declared after it
+
---
+++ model/16/16.2.js
@@ -0,0 +1,35 @@
+// block 1 
+//  10 
+// if true will change the value of x and it will be 20 
+//  20 
+//  20 takes the new value
+
+
+// block 2 
+// 10 
+// 20
+// 10
+// the iife will declare  an a new virable called also x but it will be private for function only 
+
+
+// Block 3
+// 10
+// 20 but its an a new virable not the same of out side virable
+// 30 also new virable x=30
+// 20 return to x that placed at start of func
+// 10 global virable
+
+
+
+// block 4 
+// the value of the x out side not the value of inside the func it will threw undefind
+
+// Block 5
+// undefined  // if false it will be 50 but deafult is true
+// undefined //the value declared after that 
+// 100 //value of y 
+
+
+// Block 6   
+// foo
+// error because its an a expression save in const its has to be befor we call it 
